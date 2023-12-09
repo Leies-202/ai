515f6ed353369bd18f54280746bc8092d7f06195
Leies <lei@lei202.com>
2023-12-09 23:00:16 +0900

    Update index.ts

--- src/modules/emoji-react/index.ts
+++ src/modules/emoji-react/index.ts
@@ -68,6 +68,7 @@ export default class extends Module {
 		}
 
 		if (includes(note.text, ['ぴざ'])) return react('🍕');
+		if (includes(note.text, ['プリント'])) return react('🖨');
 		if (includes(note.text, ['ぷりん'])) return react('🍮');
 		if (includes(note.text, ['うんち'])) return react('🚽');
 		if (includes(note.text, ['衣亜', '衣亜ちゃん', '衣亜たそ'])) return react('🙌');
